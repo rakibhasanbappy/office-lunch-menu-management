83a57f99230818166b747feeb8465b3cf5175544
Rakib Hasan Bappy <rakibhasan15144@gmail.com>
2024-07-02 13:49:20 +0600

    style: sign-up page design completed

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
-const reg_btn = document.querySelector(".register");
+const reg_btn = document.querySelector(".register_btn");
+const log_btn = document.querySelector(".login_btn");
 
 const login_form = document.querySelector(".login");
+const register_form = document.querySelector(".register");
+
+register_form.style.display = "none";
 
 reg_btn.addEventListener("click", () => {
   login_form.style.display = "none";
+  register_form.style.display = "flex";
+});
+
+log_btn.addEventListener("click", () => {
+  register_form.style.display = "none";
+  login_form.style.display = "flex";
 });
